86035a477f0f57f941c3bccabc18619f8bce10d6
aricahyonodev <cahyono.ari81@gmail.com>
2024-08-09 15:27:06 +0700

    delete sheet

--- app/forma/[slug]/page.js
+++ app/forma/[slug]/page.js
@@ -1,56 +1,56 @@
-import { InputFormBySearch } from "./inputFormBySearch";
-import {  SelectForm } from "./selectForm";
+// import { InputFormBySearch } from "./inputFormBySearch";
+// import {  SelectForm } from "./selectForm";
 
-const InputForm = ({ label, inpuValue, disabled = false }) => {
-  return (
-    <div className="mt-5">
-      <label className="uppercase mb-1 block font-medium">{label} </label>
-      <input
-        type="text"
-        value={inpuValue}
-        className={`border-2 p-2 rounded-md w-full ${
-          disabled && " cursor-not-allowed"
-        }`}
-        // readOnly
-        disabled={disabled}
-      />
-    </div>
-  );
-};
-
-
-function hurufKeAngka(huruf) {
-  huruf = huruf.toUpperCase(); // Mengubah huruf menjadi huruf besar
-
-  return huruf.charCodeAt(0) - "A".charCodeAt(0);
-}
+// const InputForm = ({ label, inpuValue, disabled = false }) => {
+//   return (
+//     <div className="mt-5">
+//       <label className="uppercase mb-1 block font-medium">{label} </label>
+//       <input
+//         type="text"
+//         value={inpuValue}
+//         className={`border-2 p-2 rounded-md w-full ${
+//           disabled && " cursor-not-allowed"
+//         }`}
+//         // readOnly
+//         disabled={disabled}
+//       />
+//     </div>
+//   );
+// };
 
 
-export default async function FormA({ params }) {
-  const qrCode = params.slug;
-  let data;
-  const blokNumber = hurufKeAngka(qrCode[0])
+// function hurufKeAngka(huruf) {
+//   huruf = huruf.toUpperCase(); // Mengubah huruf menjadi huruf besar
 
-  try { 
-    const revalidatedData = await fetch(
-      `/googlesheet/${blokNumber}`
-    );
-    data = await revalidatedData.json();
-  } catch (error) {
-    console.log(error);
-  }
+//   return huruf.charCodeAt(0) - "A".charCodeAt(0);
+// }
 
-  return (
-    <div className="flex justify-center">
-      <div className="bg-white border-2 w-5/6 shadow-md rounded-md pt-14 text-start px-6 ">
-        <h1 className="uppercase text-2xl font-semibold text-center mb-7">
-          form a
-        </h1>
 
-        <InputForm label={"Code"} inpuValue={qrCode} disabled={true} />
-        <SelectForm blokNumber={blokNumber} />
-        <InputFormBySearch label={"nama pemilih"} qrCode={qrCode} data={data} blokNumber={blokNumber} />
-      </div>
-    </div>
-  );
-}
+// export default async function FormA({ params }) {
+//   const qrCode = params.slug;
+//   let data;
+//   const blokNumber = hurufKeAngka(qrCode[0])
+
+//   try { 
+//     const revalidatedData = await fetch(
+//       `/googlesheet/${blokNumber}`
+//     );
+//     data = await revalidatedData.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+//   return (
+//     <div className="flex justify-center">
+//       <div className="bg-white border-2 w-5/6 shadow-md rounded-md pt-14 text-start px-6 ">
+//         <h1 className="uppercase text-2xl font-semibold text-center mb-7">
+//           form a
+//         </h1>
+
+//         <InputForm label={"Code"} inpuValue={qrCode} disabled={true} />
+//         <SelectForm blokNumber={blokNumber} />
+//         <InputFormBySearch label={"nama pemilih"} qrCode={qrCode} data={data} blokNumber={blokNumber} />
+//       </div>
+//     </div>
+//   );
+// }
